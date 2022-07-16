66809ff62f20b27fdea0fb54c16bd6cd7008895c
dmoehnke <moehnke.dalton@gmail.com>
2022-07-16 14:17:12 -0700

    started gamestate_manager and gamescreen

--- RoadToElDoRando/Prefabs/System/GameManager.gd
+++ RoadToElDoRando/Prefabs/System/GameManager.gd
@@ -1,13 +1,53 @@
 extends Node
 
-var gameState = {}
-
+var gameState = {
+	"seed":0,
+	"Relics":{},
+	"Ship":{
+		"ShipType":{},
+		"Supplies":10,
+		"Crew":{},
+		"Hull":5,
+		"Statuses":{}
+	},
+	"Round":0,
+	"PortDistance":4,
+	"PortsReached": 0,
+	"CurrentDistance":0,
+	"totalDistance": 0,
+	"events":{}
+}
+var fileString = "user://file_data.json"
+var RNG = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_game()
+	initialize()
 	pass # Replace with function body.
 
+func get_game_state():
+	return gameState
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_rng():
+	return RNG
+
+func initialize():
+	RNG.seed = gameState["seed"]
+
+func load_game():
+	var file = File.new()
+	if not file.file_exists(fileString):
+		RNG.randomize()
+		gameState["seed"] = RNG.seed
+		save()
+		return
+	file.open(fileString, File.READ)
+	gameState = parse_json(file.get_as_text())
+
+
+func save():
+	var file = File.new()
+	file.open(fileString, File.WRITE)
+	file.store_line(to_json(gameState))
+	file.close()
