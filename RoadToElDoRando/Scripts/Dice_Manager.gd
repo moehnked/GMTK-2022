25564fb4592dd5aa457e90b4fffbe5887ce940d9
Lane Thompson <46453380+thomlane@users.noreply.github.com>
2022-07-16 14:08:13 -0700

    Read dice

--- RoadToElDoRando/Scripts/Dice_Manager.gd
+++ RoadToElDoRando/Scripts/Dice_Manager.gd
@@ -12,11 +12,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
 		for die in diceArray:
+			die.mode = RigidBody.MODE_RIGID
+			die.set_sleeping(false)
 			var force_vector = Vector3.UP
 			force_vector.rotated(Vector3.LEFT, 2 * PI * randf())
 			force_vector.rotated(Vector3.UP, 2 * PI * randf())
 			force_vector.rotated(Vector3.FORWARD, 2 * PI * randf())
 			die.apply_central_impulse(force_vector * 100)
 			
+	if Input.is_action_just_pressed("finalize_roll"):
+		finalize_roll()
+			
+	if Input.is_action_just_pressed("read_value"):
+		for die in diceArray:
+			print("in Manager: ", die.read_face())
+
+func finalize_roll ():
+	for die in diceArray:
+		print("in Manager: ", die.read_face())
+	
--- RoadToElDoRando/Scripts/Dice_d6.gd
+++ RoadToElDoRando/Scripts/Dice_d6.gd
@@ -8,9 +8,17 @@ var side_raycasts = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	side_raycasts = [$Cast_1,$Cast_2,$Cast_3,$Cast_4,$Cast_5,$Cast_6]
 	pass # Replace with function body.
+	
+func _process(delta):
+	if self.is_sleeping():
+		self.mode = RigidBody.MODE_STATIC
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func read_face ():
+	for i in range(side_raycasts.size()):
+		side_raycasts[i].force_raycast_update()
+		if side_raycasts[i].is_colliding():
+			var collider = side_raycasts[i].get_collider()
+			return 7 - (i + 1)
