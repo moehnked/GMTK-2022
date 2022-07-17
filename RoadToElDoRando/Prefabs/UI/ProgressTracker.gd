1620154e1e3203b7ff99c0101afbb08e1ca38321
Devon Cash <devon.p.cash@gmail.com>
2022-07-17 01:42:35 -0700

    Exploration Manager go

--- RoadToElDoRando/Prefabs/UI/ProgressTracker.gd
+++ RoadToElDoRando/Prefabs/UI/ProgressTracker.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal progress_updated
+
 export(int) var distance_travelled setget set_distance;
 export(int) var distance_to_port;
 
@@ -11,15 +13,19 @@ var endingPoint;
 func set_progress( travelled, left ): 
 	var progress = float(travelled) / float(left);
 	var target = $StartingPoint.position.x + $StartingPoint.position.distance_to($Port.position) * progress;
+	set_distance( left - travelled);
 	$Tween.interpolate_property($Ship, 'position:x', $Ship.position.x, target, 2, Tween.TRANS_SINE)
 	$Tween.start();
 # Update the leagues to go label, tween the ship progress between ship starting point and port position
 func set_distance(value):
-	set_progress( value, distance_to_port);
-	$LeaguesToGo.set_bbcode(str('[center]' , distance_to_port - value, '[/center]'))
+	$LeaguesToGo.set_bbcode(str('[center]' , value, '[/center]'))
 	pass
+	
+func finish_update():
+	emit_signal("progress_updated");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Tween.connect('tween_all_completed', self, 'finish_update')	
 	$StartingPoint.position.y = $Port.position.y;
 	$Ship.position = $StartingPoint.position;
